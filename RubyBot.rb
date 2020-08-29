fee3e4a664ff35c3fd4670c39d0f47c077e11832
JoaoCardoso193 <63752487+JoaoCardoso193@users.noreply.github.com>
2020-08-29 19:34:48 -0400

    Update RubyBot.rb

--- RubyBot.rb
+++ RubyBot.rb
@@ -5,10 +5,10 @@ require 'literate_randomizer'
 def ruby_bot
     #Connecting to Twitter App
     client = Twitter::REST::Client.new do |config|
-        config.consumer_key = "4BUjRhKHrusqWmU5EX8p1M4HA"
-        config.consumer_secret = "J5xdmzL6oiTpFBSyAGmiMbNsCi5OUK5hC1VDFtEWDswS7fp3zH"
-        config.access_token = "1275089697438674945-VNMzuAHBnuBH8h9pwI4S5s500j7EcI"
-        config.access_token_secret = "5x7irfuAM7lgcfP9DKRE0nPtapvt0ybKV3voRs4J5N3iI"
+        config.consumer_key = ""
+        config.consumer_secret = ""
+        config.access_token = ""
+        config.access_token_secret = ""
     end
 
     #tweets random sentence with random hashtag when app is called, at the :00 and :30 minutes marks
